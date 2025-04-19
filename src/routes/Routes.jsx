75ae9f0b23d42fd967b01f4093a11c26bedc746e
Mintu Sikder <mintusikder15@gmail.com>
2025-04-19 16:12:17 +0600

    added page & component

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import { createBrowserRouter } from 'react-router';
-import Home from '../pages/Home';
-import Root from '../root/Root';
+import React from "react";
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home";
+import Root from "../root/Root";
+import Favorites from "../pages/Favorites";
+import About from "../pages/About";
+import ErrorPages from "../pages/ErrorPages";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root></Root>,
-      errorElement : <p>Error 404</p>,
-      children :[
-        {
-          index : true,
-          Component : Home
-        }
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPages></ErrorPages>,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/favorites",
+        Component: Favorites,
+      },
+      {
+        path: "/about",
+        Component: About,
+      },
+    ],
+  },
+]);
